5a97fbb9c6d2626f6a98b3f04a62d5259067319a
Alex Bielen <alexhendriebielen@gmail.com>
2016-11-16 21:03:07 -0500

    Directory structure

--- make_dirs.py
+++ make_dirs.py
@@ -3,8 +3,10 @@
 import os
 
 for name in ['chanan', 'alex']:
-    for i in range(0, 26):
+    for i in range(1, 27):
         dir_name = os.path.join(str(i).zfill(2) + "_lecture", name)
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
+            with open(os.path.join(dir_name, 'README.md'), 'w') as f:
+                    f.write("Notes for lecture {}".format(i))
 
